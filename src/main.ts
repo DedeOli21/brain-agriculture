421c07bc761623f2161ce812ca7dc2436824085f
David Oliveira <dedepco@gmail.com>
2025-03-04 21:30:52 -0300

    feat: add Docker configuration for app service and integrate logging with NestJS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(Logger));
+
+  setupSwagger(app);
+  
   await app.listen(3000);
 }
 bootstrap();
+function setupSwagger(app: INestApplication<any>) {
+  throw new Error('Function not implemented.');
+}
+
