e69ddc29dc0fe06ed363eaae7b3cc5b63ae69c0b
David Oliveira <dedepco@gmail.com>
2025-03-07 09:55:50 -0300

    feat: update database configuration to support DATABASE_URL and enable SSL

--- src/shared/databases/database.config.ts
+++ src/shared/databases/database.config.ts
@@ -7,17 +7,20 @@ export class DataBaseConnectionService implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
 
   createTypeOrmOptions(): TypeOrmModuleOptions {
+    const databaseUrl = this.configService.get<string>('DATABASE_URL');
+
     return {
-      name: 'default',
       type: 'postgres',
-      url: this.configService.get<string>('DATABASE_URL'),
-      host: this.configService.get<string>('TYPEORM_HOST'),
-      port: Number(this.configService.get<number>('TYPEORM_PORT')),
-      username: this.configService.get<string>('TYPEORM_USERNAME'),
-      password: this.configService.get<string>('TYPEORM_PASSWORD'),
-      database: this.configService.get<string>('TYPEORM_DATABASE'),
+      url: databaseUrl || undefined,
+      host: databaseUrl ? undefined : this.configService.get<string>('TYPEORM_HOST'),
+      port: databaseUrl ? undefined : Number(this.configService.get<number>('TYPEORM_PORT')),
+      username: databaseUrl ? undefined : this.configService.get<string>('TYPEORM_USERNAME'),
+      password: databaseUrl ? undefined : this.configService.get<string>('TYPEORM_PASSWORD'),
+      database: databaseUrl ? undefined : this.configService.get<string>('TYPEORM_DATABASE'),
       entities: [this.configService.get<string>('TYPEORM_ENTITIES')],
-      synchronize: this.configService.get<boolean>('TYPEORM_SYNCHRONIZE'),
+      synchronize: this.configService.get<boolean>('TYPEORM_SYNCHRONIZE', false),
+      migrationsRun: true,
+      ssl: databaseUrl ? { rejectUnauthorized: false } : undefined,
     };
   }
 }
