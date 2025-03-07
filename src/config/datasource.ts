60ab24a0d9406101f0f850ed39cda34516b5355c
David Oliveira <dedepco@gmail.com>
2025-03-07 11:00:38 -0300

    feat: update datasource configuration to disable synchronization, enhance DATABASE_URL support, and adjust SSL settings

--- src/config/datasource.ts
+++ src/config/datasource.ts
@@ -1,28 +1,30 @@
-import { Crop } from '@domain/entities/crops/crop.entity';
-import { Farm } from '@domain/entities/farms/farm.entity';
-import { Harvest } from '@domain/entities/harvest/harvest.entity';
-import { Producer } from '@domain/entities/producers/producer.entity';
-import { Season } from '@domain/entities/season/season.entity';
 import { DataSource } from 'typeorm';
+import { config } from 'dotenv';
+import { Producer } from '@domain/entities/producers/producer.entity';
+import { Farm } from '@domain/entities/farms/farm.entity';
+import { Crop } from '@domain/entities/crops/crop.entity';
+import { Season } from '@domain/entities/season/season.entity';
+import { Harvest } from '@domain/entities/harvest/harvest.entity';
 
-console.log('Entities being loaded:', [Producer, Farm, Crop, Harvest, Season]);
+config();
+
+console.log('Entities being loaded:', [Producer, Farm, Crop, Season, Harvest]);
 
 const datasource = new DataSource({
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 5432,
-  username: process.env.DB_USERNAME || 'admin',
-  password: process.env.DB_PASSWORD || 'password',
-  database: process.env.DB_DATABASE || 'brain_agriculture',
+  url: process.env.DATABASE_URL || undefined,
+  host: process.env.DATABASE_URL ? undefined : process.env.DB_HOST || 'localhost',
+  port: process.env.DATABASE_URL ? undefined : Number(process.env.DB_PORT) || 5432,
+  username: process.env.DATABASE_URL ? undefined : process.env.DB_USERNAME || 'admin',
+  password: process.env.DATABASE_URL ? undefined : process.env.DB_PASSWORD || 'password',
+  database: process.env.DATABASE_URL ? undefined : process.env.DB_DATABASE || 'brain_agriculture',
   entities: [Producer, Farm, Crop, Season, Harvest],
   migrations: ['dist/migrations/*.js'],
   synchronize: false,
   migrationsRun: true,
-  ssl:
-    process.env.NODE_ENV === 'production'
-      ? { rejectUnauthorized: false }
-      : false,
+  ssl: process.env.DATABASE_URL
+    ? { rejectUnauthorized: false } // Railway exige SSL
+    : false,
 });
 
 export default datasource;
