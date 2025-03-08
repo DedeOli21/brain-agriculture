76ad59da6b6dbc89876e3a548867b056059a078e
David Oliveira <dedepco@gmail.com>
2025-03-08 02:32:49 -0300

    feat: enhance CreateFarmRequestDto with ApiProperty decorators for improved Swagger documentation

--- src/presentation/controllers/farms/dto/request/create-request.dto.ts
+++ src/presentation/controllers/farms/dto/request/create-request.dto.ts
@@ -1,30 +1,69 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateFarmRequestDto {
+  @ApiProperty({
+    description: 'Farm name',
+    example: 'Farm name',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   name: string;
 
+  @ApiProperty({
+    description: 'Farm city',
+    example: 'Farm city',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   city: string;
 
+  @ApiProperty({
+    description: 'Farm state',
+    example: 'Farm state',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   state: string;
 
+  @ApiProperty({
+    description: 'Farm total area',
+    example: 100,
+    type: Number,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Min(0)
   totalArea: number;
 
+  @ApiProperty({
+    description: 'Farm arable area',
+    example: 100,
+    type: Number,
+  })
   @IsNotEmpty()
   @IsNumber()
   @Min(0)
   arableArea: number;
 
+  @ApiProperty({
+    description: 'Farm vegetation area',
+    example: 100,
+    type: Number,
+  })
+  @IsNotEmpty()
+  @IsNumber()
+  @Min(0)
   vegetationArea: number;
 
+  @ApiProperty({
+    description: 'id corresponding to the producer',
+    example: 'b4sa-saehr31-3r1-ee12',
+    type: String,
+  })
   @IsNotEmpty()
   @IsString()
   producerId: string;
